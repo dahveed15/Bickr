7af5e37ce88df1ef544d253c05efe3b9c1fa8077
David Harris <David Harris.dahveed15@gmail.com>
2018-02-08 13:46:32 -0800

    add more styling to login nav bar. Work on button

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -10,9 +10,9 @@ export default ({currentUser, logout, pathName }) => {
         <header className="login-nav-bar">
           <div className="login-nav-bar-left">
             <Link to="/profile" className="logo">bickr</Link>
-            <Link to="/profile">You</Link>
-            <Link to="/profile">Explore</Link>
-            <Link to="/albums/new">Create</Link>
+            <Link to="/profile" className="you">You</Link>
+            <Link to="/profile" className="explore">Explore</Link>
+            <Link to="/albums/new" className="create">Create</Link>
           </div>
           <div className="login-nav-bar-right">
             <Link to="/albums">Albums</Link>
